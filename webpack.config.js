533526c7b596cf2fdb4e445eb3b6dde6715373a1
Caesar Bell <caesar.jd.bell@gmail.com>
2020-02-21 15:52:29 -0500

    :wrench: setup webpack to handle flow and es2017 methods

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+const path = require("path");
+const FlowBabelWebpackPlugin = require("flow-babel-webpack-plugin");
+
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: `${__dirname}/build`,
+    path: path.resolve("dist"),
+    libraryTarget: "commonjs2",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [new FlowBabelWebpackPlugin()],
 };
